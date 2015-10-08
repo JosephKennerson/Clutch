c89eac8d656d89c51fccc9fa8d3ab249ef34e51d
Madison Sites <madison.sites@gmail.com>
2015-10-08 16:56:58 -0700

    Add user resource

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :username, :email, :address_line_1, :address_line_2, :city, :state, :zip, :password, :photo, :first_name, :last_name, :created_at, :updated_at
---
+++ db/migrate/20151008235453_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :address_line_1
+      t.string :address_line_2
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :password
+      t.string :photo
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps null: false
+    end
+  end
+end
