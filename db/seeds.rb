e13bcfd207e68434fb1b295ceb4273485113339a
Madison Sites <madison.sites@gmail.com>
2015-10-09 16:55:40 -0700

    pre-merge commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do
+  random_size = rand(1..10)
+  Event.create!(
+        public_location: Faker::University.name,
+        address_line_1: Faker::Address.street_address,
+        address_line_2: Faker::Address.secondary_address,
+        city: Faker::Address.city,
+        state: Faker::Address.state_abbr,
+        zip: Faker::Address.zip,
+        max_size: random_size,
+        time_start: Faker::Time.between(DateTime.now - 1, DateTime.now),
+        time_end: Faker::Time.forward(1),
+        name: ,
+        description: ,
+        category: ,
+        approval_required: ,
+    )
+
+end
