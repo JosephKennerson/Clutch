57e46ececb983d29c6e9a4a4b620aa35de90531f
Madison Sites <madison.sites@gmail.com>
2015-10-12 12:21:08 -0700

    Testing gem mods and host events profile link working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,6 +71,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def hosted_events
+    @user = User.find(params[:id])
+  end
+
+  def guest_events
+    @user = User.find(params[:id])
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
