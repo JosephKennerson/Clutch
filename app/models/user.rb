721cdb23323b646be27554fcdd5cf15b41949fac
JosephKennerson <JosephMKennerson@gmail.com>
2015-10-13 18:06:10 -0700

    Fix callback data error

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,13 +21,23 @@ class User < ActiveRecord::Base
   #   self.ratings.where(ratee: self.id)
   # end
 
+# ***** Original Facebook Login Auth ******
+  # def self.from_omniauth(auth)
+  #     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #       user.provider = auth.provider
+  #       user.uid = auth.uid
+  #       user.email = auth.info.email
+  #       user.password = Devise.friendly_token[0,20]
+  #     end
+  # end
+
+# ***** Facebook Login Auth ******
   def self.from_omniauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.provider = auth.provider
-        user.uid = auth.uid
-        user.email = auth.info.email
-        user.password = Devise.friendly_token[0,20]
-      end
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.email = auth.info.email || "#{auth.info.nickname}@facebook.com"
+    user.password = Devise.friendly_token[0,20]
+    user.name = auth.info.name   # assuming the user model has a name
   end
+end
 
 end
