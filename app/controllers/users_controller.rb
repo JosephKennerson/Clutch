6d06177009ebb3b048bc905cb8d3b04b813a8345
JosephKennerson <JosephMKennerson@gmail.com>
2015-10-14 18:48:25 -0700

    Fix mandatory update password and auto logout after on profile update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,6 +41,10 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
+      if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+        params[:user].delete(:password)
+        params[:user].delete(:password_confirmation)
+      end
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
