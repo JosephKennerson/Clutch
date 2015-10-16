5e29400bf462c740523fa53beb2487dfd215fc7a
JosephKennerson <JosephMKennerson@gmail.com>
2015-10-15 23:03:15 -0700

    add all seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -648,54 +648,6 @@ User.create(
         phone_number: "617-671-8206"
     )
 
-                                User.create!(
-        username: "juliagra",
-        address_line_1: "633 Folsom",
-        address_line_2: "6th Floor",
-        city: "San Francisco",
-        state: "CA",
-        zip: "94107",
-        email: "Julia@google.com",
-        password: "adminadmin",
-        avatar: "https://avatars2.githubusercontent.com/u/12245008?v=3&s=400",
-        first_name: "Julia",
-        last_name: "Graber",
-        phone_number: "617-671-8206"
-    )
-
-                                                User.create!(
-        username: "juliagra",
-        address_line_1: "633 Folsom",
-        address_line_2: "6th Floor",
-        city: "San Francisco",
-        state: "CA",
-        zip: "94107",
-        email: "Julia@google.com",
-        password: "adminadmin",
-        avatar: "https://avatars2.githubusercontent.com/u/12245008?v=3&s=400",
-        first_name: "Julia",
-        last_name: "Graber",
-        phone_number: "617-671-8206"
-    )
-
-                                                                User.create!(
-        username: "juliagra",
-        address_line_1: "633 Folsom",
-        address_line_2: "6th Floor",
-        city: "San Francisco",
-        state: "CA",
-        zip: "94107",
-        email: "Julia@google.com",
-        password: "adminadmin",
-        avatar: "https://avatars2.githubusercontent.com/u/12245008?v=3&s=400",
-        first_name: "Julia",
-        last_name: "Graber",
-        phone_number: "617-671-8206"
-    )
-
-
-
-
 # # Admin events
 
 # 5.times do
@@ -714,7 +666,7 @@ User.create(
 #         description: FFaker::HipsterIpsum.phrase,
 #         category: FFaker::Sport.name,
 #         approval_required: FFaker::Boolean.sample,
-#         host_id: 1,
+#         host_id: rand(-122.507367...-122.389335),
 #         status: FFaker::Boolean.sample,
 #         longitude: rand(-122.507367...-122.389335),
 #         latitude: rand(37.735722...37.807791)
@@ -724,28 +676,28 @@ User.create(
 
 # # random events
 
-# 25.times do
-#   random_size = rand(1..10)
-#   random_user = rand(1..User.count)
-#   Event.create!(
-#         public_location: FFaker::Venue.name,
-#         address_line_1: FFaker::AddressUS.street_address,
-#         address_line_2: FFaker::AddressUS.secondary_address,
-#         city: FFaker::AddressUS.city,
-#         state: FFaker::AddressUS.state_abbr,
-#         zip: FFaker::AddressUS.zip_code,
-#         max_size: random_size,
-#         time_start: Faker::Time.between(DateTime.now - 1, DateTime.now),
-#         time_end: Faker::Time.forward(1),
-#         name: FFaker::Company.catch_phrase,
-#         description: Faker::Hacker.say_something_smart,
-#         category: FFaker::Sport.name,
-#         approval_required: FFaker::Boolean.sample,
-#         host_id: random_user,
-#         status: FFaker::Boolean.sample,
-#         longitude: rand(-122.507367...-122.389335),
-#         latitude: rand(37.735722...37.807791)
-#     )
+50.times do
+  random_size = rand(1..10)
+  random_user = rand(1..User.count)
+  Event.create!(
+        public_location: FFaker::Venue.name,
+        address_line_1: FFaker::AddressUS.street_address,
+        address_line_2: FFaker::AddressUS.secondary_address,
+        city: FFaker::AddressUS.city,
+        state: FFaker::AddressUS.state_abbr,
+        zip: FFaker::AddressUS.zip_code,
+        max_size: random_size,
+        time_start: Faker::Time.between(DateTime.now - 1, DateTime.now),
+        time_end: Faker::Time.forward(1),
+        name: FFaker::Company.catch_phrase,
+        description: Faker::Hacker.say_something_smart,
+        category: FFaker::Sport.name,
+        approval_required: FFaker::Boolean.sample,
+        host_id: random_user,
+        status: FFaker::Boolean.sample,
+        longitude: rand(-122.507367...-122.389335),
+        latitude: rand(37.735722...37.807791)
+    )
 
 # end
 
@@ -765,31 +717,31 @@ User.create(
 # # random comments (public and private)
 
 
-# 35.times do
-#   random_event = rand(1..Event.count)
-#   random_user = rand(1..User.count)
-#   Comment.create!(
-#         event_id: random_event,
-#         user_id: random_user,
-#         body: FFaker::BaconIpsum.phrase,
-#         is_private: FFaker::Boolean.sample
-#     )
+100.times do
+  random_event = rand(1..Event.count)
+  random_user = rand(1..User.count)
+  Comment.create!(
+        event_id: random_event,
+        user_id: random_user,
+        body: FFaker::BaconIpsum.phrase,
+        is_private: FFaker::Boolean.sample
+    )
 
-# end
+end
 
 # # random pending rsvps (id # 1-25)
 
-# 20.times do
-#   random_guest = rand(10..User.count)
-#   random_event = rand(1..Event.count)
-#   new_event = Rsvp.create!(
-#       guest_id: random_guest,
-#       event_id: random_event,
-#       message: Faker::Hacker.say_something_smart,
-#       pending: true
-#     )
+100.times do
+  random_guest = rand(10..User.count)
+  random_event = rand(1..Event.count)
+  new_event = Rsvp.create!(
+      guest_id: random_guest,
+      event_id: random_event,
+      message: Faker::Hacker.say_something_smart,
+      pending: true
+    )
 
-# end
+end
 
 # #Admin pending rsvps
 
@@ -821,69 +773,69 @@ User.create(
 
 # # random not-pending rsvps (true and false; id #26-50)
 
-# 30.times do
+100.times do
 
-#   random_guest = rand(10..User.count)
-#   random_host = rand(1..9)
-#   random_event = rand(1..Event.count)
-#   new_event = Rsvp.create!(
-#       guest_id: random_guest,
-#       event_id: random_event,
-#       pending: false,
-#       message: Faker::Hacker.say_something_smart,
-#       confirmed: FFaker::Boolean.sample
-#     )
+  random_guest = rand(10..User.count)
+  random_host = rand(1..9)
+  random_event = rand(1..Event.count)
+  new_event = Rsvp.create!(
+      guest_id: random_guest,
+      event_id: random_event,
+      pending: false,
+      message: Faker::Hacker.say_something_smart,
+      confirmed: FFaker::Boolean.sample
+    )
 
-# end
+end
 
-# # random confirmed rsvps (id #51-75)
+# random confirmed rsvps (id #51-75)
 
 
-# 35.times do
-#   random_guest = rand(10..User.count)
-#   random_host = rand(1..9)
-#   random_event = rand(1..Event.count)
-#   new_event = Rsvp.create!(
-#       guest_id: random_guest,
-#       event_id: random_event,
-#       pending: false,
-#       message: Faker::Hacker.say_something_smart,
-#       confirmed: true
-#     )
+100.times do
+  random_guest = rand(10..User.count)
+  random_host = rand(1..9)
+  random_event = rand(1..Event.count)
+  new_event = Rsvp.create!(
+      guest_id: random_guest,
+      event_id: random_event,
+      pending: false,
+      message: Faker::Hacker.say_something_smart,
+      confirmed: true
+    )
 
-# end
+end
 
 # # random ratings (from confirmed rsvps)
 
-# 35.times do
-#   count = 1
-#   current_event = Event.find(count)
-#   random_guest = current_event.guests.sample
-#       Rating.create!(
-#           event_id: count,
-#           rating: rand(1.0..5.0),
-#           rating_feedback: FFaker::HipsterIpsum.phrase,
-#           rater_id: current_event.host.id,
-#           ratee_id: random_guest.id
-#         )
-#     count += 1
-# end
+200.times do
+  count = 1
+  current_event = Event.find(count)
+  random_guest = current_event.guests.sample
+      Rating.create!(
+          event_id: count,
+          rating: rand(1.0..5.0),
+          rating_feedback: FFaker::HipsterIpsum.phrase,
+          rater_id: current_event.host.id,
+          ratee_id: random_guest.id
+        )
+    count += 1
+end
 
 # # random ratings (switched roles)
 
 
-# 35.times do
-#   count = 1
-#   current_event = Event.find(count)
-#       Rating.create!(
-#           event_id: count,
-#           rating: rand(1.0..5.0),
-#           rating_feedback: FFaker::HipsterIpsum.phrase,
-#           ratee_id: current_event.host.id,
-#           rater_id: current_event.guests.sample.id
-#         )
-#     count += 1
-# end
+150.times do
+  count = 1
+  current_event = Event.find(count)
+      Rating.create!(
+          event_id: count,
+          rating: rand(1.0..5.0),
+          rating_feedback: FFaker::HipsterIpsum.phrase,
+          ratee_id: current_event.host.id,
+          rater_id: current_event.guests.sample.id
+        )
+    count += 1
+end
 
 
 
